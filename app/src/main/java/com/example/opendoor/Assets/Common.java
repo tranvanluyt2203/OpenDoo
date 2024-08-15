e896272e74a4c9c5514c6bf3148e6e358f108ba4
@tranvanluyt22032002 <luyttran@rion-lab.com>
2024-08-16 00:54:47 +0700

    init

---
+++ app/src/main/java/com/example/opendoor/Assets/Common.java
@@ -0,0 +1,13 @@
+package com.example.opendoor.Assets;
+
+public class Common {
+    public static final String TAG = "Tag view";
+
+    public static final String FOLDER_ANIMATION = "Animation";
+
+    public static final String LOADING_DOT = "loading_dot.json";
+    public static final String LOADING_ROUND = "loading_round.json";
+    public static final String OPEN_DOOR = "open_door.json";
+    public static final String SCAN_FACE = "scan_face.json";
+    public static final String OPEN_LOCK = "open_lock.json";
+}
